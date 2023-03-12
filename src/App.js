595db149d19844f1c500a7a74465e3d187e7e1b2
majapiszczek <piszczekmaja33@gmail.com>
2023-03-12 15:32:26 +0100

    added api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
 function App() {
+  const apiKey = "sDb8iiEZI57VCsfkNMl0Gk8XVhPtgkNQg9jxlHK1";
+  const apiUrl = `https: //api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`;
+
   return (
     <div className="App">
       <h1>hello</h1>
+      <a href={apiUrl}>link</a>
     </div>
   );
 }
